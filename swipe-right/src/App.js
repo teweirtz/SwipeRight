d6665deeb9b01292ec5126a27c060ee15c927155
Taylor Weirtz <teweirtz@gmail.com>
2023-05-08 06:57:31 -0400

    installed material icons

--- swipe-right/src/App.js
+++ swipe-right/src/App.js
@@ -1,11 +1,12 @@
+import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>SwipeRightForEmployment</h1>
 
-      {/* Header */}
+      < Header />
       {/* Profiles */}
       {/* Buttons */}
     
--- swipe-right/src/Header.js
+++ swipe-right/src/Header.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import './Header.css';
+import PersonIcon from '@mui/icons-material/Person';
+import IconButton from '@mui/material';
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className="Header">
+      <IconButton>
+        <PersonIcon  fontSize="large" className="header_icon"/>
+      </IconButton>
+
+      <img
+        className="header_logo"
+        src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
+        alt=""
+        />
+    </div>
   )
 }
 
-export default Header
+export default Header;
