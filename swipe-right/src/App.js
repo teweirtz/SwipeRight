365cd7de1d988e17db7336758f556f03d0e90f74
Taylor Weirtz <teweirtz@gmail.com>
2023-05-14 15:38:55 -0400

    started on buttons in js file

--- swipe-right/src/App.js
+++ swipe-right/src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Profiles from './Profiles';
+import Buttons from './Buttons';
 
 function App() {
   return (
     <div className="App">
 
-      < Header />
+      <Header />
       <Profiles />
-      {/* Buttons */}
+      <Buttons />
     
     </div>
   );
