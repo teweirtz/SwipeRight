fb3a7ec8a577748a9db8f3b4dd781530d50c8254
Taylor Weirtz <teweirtz@gmail.com>
2023-05-14 15:05:14 -0400

    temporary fix for logo

--- swipe-right/src/Header.js
+++ swipe-right/src/Header.js
@@ -3,7 +3,7 @@ import './Header.css';
 import PersonIcon from '@mui/icons-material/Person';
 import {IconButton} from '@mui/material';
 import ForumIcon from '@mui/icons-material/Forum';
-import logo from './Tinder_logo_PNG6.png'
+// import logo from './Tinder_logo_PNG6.png'
 
 function Header() {
   return (
@@ -11,7 +11,7 @@ function Header() {
       <IconButton>
         <PersonIcon  fontSize="large" className="header__icon"/>
       </IconButton>
-        {/* <img className="header__logo" src={logo} alt="Tinder logo"/> */}
+        <img className="header__logo" src="https://1000logos.net/wp-content/uploads/2019/06/tinder-logo.jpg" alt="Tinder logo"/>
       <IconButton>
         <ForumIcon fontSize="large" className="header__icon"/>
       </IconButton>
