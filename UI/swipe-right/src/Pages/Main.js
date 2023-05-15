c4fbd9992accee836031af3726c24611f09c5210
Taylor Weirtz <teweirtz@gmail.com>
2023-05-15 19:52:29 -0400

    added functionality to sign up button

--- UI/swipe-right/src/Pages/Main.js
+++ UI/swipe-right/src/Pages/Main.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
 function Main() {
+    const authToken=true
+    const handleClick = () => {
+        console.log('click')
+    }
   return (
-    <div Main>
-    <button>SignUp</button>
+    <div className="Main">
+    <button classname="signUpButton" onClick={handleClick}>
+        {authToken ? 'Signout' : 'Create Account'}
+    </button>
     </div>
   )
 }
 
-export default Main
+export default Main;
