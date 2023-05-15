bed90bcc408352a46751bf05ef112978c2b70726
Taylor Weirtz <teweirtz@gmail.com>
2023-05-15 18:21:42 -0400

    server crashed- fixed port in all caps typo

--- Server/index.js
+++ Server/index.js
@@ -5,4 +5,4 @@ const port = 8000
 app.get('/', (req, res) => {
     res.json('Hello World')
 })
-app.listen(PORT, () => console.log('Server running on port' + port))
+app.listen(port, () => console.log('Server running on port' + port))
