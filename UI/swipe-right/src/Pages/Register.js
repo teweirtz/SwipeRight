2cb2a9859f66ccfba16504f523143159c7b2a695
Taylor Weirtz <teweirtz@gmail.com>
2023-05-18 17:45:32 -0400

    added gender

--- UI/swipe-right/src/Pages/Register.js
+++ UI/swipe-right/src/Pages/Register.js
@@ -2,6 +2,12 @@ import React, {useState} from 'react'
 import Navigation from '../Components/Navigation'
 
 const Register = () => {
+    const handleSubmit = () => {
+        console.log('submit')
+    }
+    const handleChange = () => {
+        console.log('submit')
+    }
     return (
         <>
         <Navigation 
@@ -25,6 +31,7 @@ const Register = () => {
                         />
 
                     <label>Birthday</label>
+                    <div className='form__container'>
                     <input
                         id="dob_day"
                         type="number"
@@ -52,9 +59,35 @@ const Register = () => {
                         value={""}
                         onChange={handleChange}
                         />
+                    </div>
+
+                    <label>Gender</label>
+                    <div className='form__container'>
+                    <input
+                        id="man__gender__identity"
+                        type="radio"
+                        name="gender__identity"
+                        value="man"
+                        onChange={handleChange}
+                        checked={false}
+                        />
+                    <label htmlFor="man__gender__identity">Man</label>
+                    <input
+                        id="woman__gender__identity"
+                        type="radio"
+                        name="gender__identity"
+                        value="woman"
+                        onChange={handleChange}
+                        checked={false}
+                        />
+                    <label htmlFor="woman__gender__identity">Woman</label>
+
+                        </div>
                 </section>
             </form>
         </div>
         </>
     )
 }
+
+export default Register;
