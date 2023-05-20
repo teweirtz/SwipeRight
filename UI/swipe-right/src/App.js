929ff19ddc324e2c2fafdc89a46907b463cb01f2
Taylor Weirtz <teweirtz@gmail.com>
2023-05-20 17:17:44 -0400

    set up profile page, installed tinder card framework

--- UI/swipe-right/src/App.js
+++ UI/swipe-right/src/App.js
@@ -1,7 +1,4 @@
 import React from 'react';
-// import Header from './Pages/Header';
-// import Profiles from './Pages/Profiles';
-// import Buttons from './Pages/Buttons';
 import Main from './Pages/Main';
 import Register from './Pages/Register';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
@@ -12,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Main/>}/>
         <Route path="/register" element={<Register/>}/>
+        <Route path="/profile" element={<Profile/>}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ UI/swipe-right/src/Pages/Profile.js
@@ -0,0 +1,43 @@
+import React from "react";
+import TinderCard from 'react-tinder-card';
+import './Profile.css'
+ 
+const Profile = () => {
+    const characters = db
+    const [lastDirection, setLastDirection] = useState()
+
+
+    const swiped = (direction, nameToDelete) => {
+        console.log('removing:' + nameToDelete)
+        setLastDirection(direction)
+    }
+
+    const outOfFrame = (name) => {
+        console.log(name + ' left the screen!')
+    }
+
+    return (
+        <div className="profile">
+            <MessageContainer/>
+            <div className = "swipe_container">
+                <div className="card_container">
+                {filteredGenderedUsers?.map((genderedUser) =>
+                            <TinderCard
+                                className="swipe"
+                                key={genderedUser.user_id}
+                                onSwipe={(dir) => swiped(dir, genderedUser.user_id)}
+                                onCardLeftScreen={() => outOfFrame(genderedUser.first_name)}>
+                                <div
+                                    style={{backgroundImage: "url(" + genderedUser.url + ")"}}
+                                    className="card">
+                                    <h3>{genderedUser.first_name}</h3>
+                                </div>
+                            </TinderCard>
+                )}
+                </div>
+            </div>
+        </div>
+    )
+ }
+
+export default Profile;
