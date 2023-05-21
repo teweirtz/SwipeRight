bcb39f7e0eb8cfc2e6c0654be4442c1ec6277b6f
Taylor Weirtz <teweirtz@gmail.com>
2023-05-20 21:16:28 -0400

    getting users from the database

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,28 @@
 const express = require('express')
 const app = express()
 const port = 8000
+const { MongoClient } = require('mongodb')
+const uri = 'mongodb+srv://teweirtz:Ammo,dawg1@cluster0.tkes3cu.mongodb.net/Cluster0?retryWrites=true&w=majority'
 
 app.get('/', (req, res) => {
     res.json('Hello World')
+})
+
+app.post('/register', (req, res) => {
+    res.json('Hello World')
+})
+app.get('/users', async (req, res) => {
+    const client = new MongoClient(uri)
+
+    try {
+        await client.connect()
+        const database = client.db('app-data')
+        const users = database.collection('users')
+
+        const returnedUsers = await users.find().toArray()
+        res.send(returnedUsers)
+    } finally {
+        await client.close()
+    }
 })
 app.listen(port, () => console.log('Server running on port' + port))
