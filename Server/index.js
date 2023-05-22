53931b479f96489bd8cbc4b0448338b94db4817f
Taylor Weirtz <teweirtz@gmail.com>
2023-05-22 18:32:42 -0400

    log into database

--- Server/index.js
+++ Server/index.js
@@ -44,11 +44,43 @@ app.post('/register', async (req, res) => {
         const token = jwt.sign(insertedUser, sanitizedEmail, {
             expiresIn: 60 *24,
         })
-        res.status(201).json({token, userId: generatedUserId, email: sanitizedEmail})
+        res.status(201).json({token, userId: generatedUserId})
     } catch (err) {
         console.log(err)
+    } finally {
+        await client.close()
     }
 })
+
+app.post('/login', async (req, res) => {
+    const client = new MongoClient(uri)
+    const {email, password} = req.body
+
+    try {
+        await client.connect()
+        const database = client.db('app-data')
+        const users = database.collection('users')
+
+        const user = await users.findOne({email})
+
+        const correctPassword = await bcrypt.compare(password, user.hashed_password)
+
+        if (user && correctPassword) {
+            const token = jwt.sign(user, email, {
+                expiresIn: 60 * 24
+            })
+            res.status(201).json({token, userId: user.user_id})
+        }
+
+        res.status(400).json('Invalid Credentials')
+
+    } catch (err) {
+        console.log(err)
+    } finally {
+        await client.close()
+    }
+})
+
 app.get('/users', async (req, res) => {
     const client = new MongoClient(uri)
 
