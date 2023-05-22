4380e52755e3230e0528a93de53f5d03caba60c1
Taylor Weirtz <teweirtz@gmail.com>
2023-05-22 19:40:09 -0400

    update user in database

--- Server/index.js
+++ Server/index.js
@@ -147,6 +147,41 @@ app.get('/gendered-users', async (req, res) => {
     }
 })
 
+app.put('/user', async (req, res) => {
+    const client = new MongoClient(uri)
+    const formData = req.body.formData
+
+    try {
+        await client.connect()
+        const database = client.db('app-data')
+        const users = database.collection('users')
+
+        const query = {user_id: formData.user_id}
+
+        const updateDocument = {
+            $set: {
+                first_name: formData.first_name,
+                dob_day: formData.dob_day,
+                dob_month: formData.dob_month,
+                dob_year: formData.dob_year,
+                show_gender: formData.show_gender,
+                gender_identity: formData.gender_identity,
+                gender_interest: formData.gender_interest,
+                url: formData.url,
+                about: formData.about,
+                matches: formData.matches
+            },
+        }
+
+        const insertedUser = await users.updateOne(query, updateDocument)
+
+        res.json(insertedUser)
+
+    } finally {
+        await client.close()
+    }
+})
+
 app.get('/users', async (req, res) => {
     const client = new MongoClient(uri)
 
