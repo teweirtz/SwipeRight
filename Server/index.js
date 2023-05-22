8c25e150f44c963a443e27e3261ab74d6db69b11
Taylor Weirtz <teweirtz@gmail.com>
2023-05-22 19:47:51 -0400

    get message from userid and add messages to database

--- Server/index.js
+++ Server/index.js
@@ -182,6 +182,41 @@ app.put('/user', async (req, res) => {
     }
 })
 
+app.get('/messages', async (req, res) => {
+    const {userId, correspondingUserId} = req.query
+    const client = new MongoClient(uri)
+
+    try {
+        await client.connect()
+        const database = client.db('app-data')
+        const messages = database.collection('messages')
+
+        const query = {
+            from_userId: userId, to_userId: correspondingUserId
+        }
+        const foundMessages = await messages.find(query).toArray()
+        res.send(foundMessages)
+    } finally {
+        await client.close()
+    }
+})
+
+app.post('/message', async (req, res) => {
+    const client = new MongoClient(uri)
+    const message = req.body.message
+
+    try {
+        await client.connect()
+        const database = client.db('app-data')
+        const messages = database.collection('messages')
+
+        const insertedMessage = await messages.insertOne(message)
+        res.send(insertedMessage)
+    } finally {
+        await client.close()
+    }
+})
+
 app.get('/users', async (req, res) => {
     const client = new MongoClient(uri)
 
