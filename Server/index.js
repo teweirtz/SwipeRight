e268a1fd637d0ce8ab223c5cbbc309f03c2b7be3
Taylor Weirtz <teweirtz@gmail.com>
2023-05-22 17:57:58 -0400

    adding new users to database

--- Server/index.js
+++ Server/index.js
@@ -1,15 +1,53 @@
 const express = require('express')
-const app = express()
 const port = 8000
 const { MongoClient } = require('mongodb')
+const { v4:uuid4 } = require('uuid')
+const jwt = require('jsonwebtoken')
+const cors = require('cors')
 const uri = 'mongodb+srv://teweirtz:Ammo,dawg1@cluster0.tkes3cu.mongodb.net/Cluster0?retryWrites=true&w=majority'
+const bcrypt = require('bcrypt')
+
+const app = express()
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.json('Hello World')
 })
 
-app.post('/register', (req, res) => {
-    res.json('Hello World')
+app.post('/register', async (req, res) => {
+    const client = new MongoClient(uri)
+    console.log(req.body)
+    const { email, password } = req.body
+    const generateduserId = uuid4()
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    try {
+        await client.connect()
+        const database = client.db('app-data')
+        const users = database.collection('users')
+
+        const existingUser = await users.findOne({email})
+
+        if (existingUser) {
+            return res.status(409).send('user already exists')
+        }
+
+        const sanitizedEmail = email.toLowerCase()
+
+        const data = {
+            user_id: generateduserId,
+            email: sanitizedEmail,
+            hashed_password: hashedPassword
+        }
+        const insertedUser = await users.insertOne(data)
+
+        const token = jwt.sign(insertedUser, sanitizedEmail, {
+            expiresIn: 60 *24,
+        })
+        res.status(201).json({token, userId: generatedUserId, email: sanitizedEmail})
+    } catch (err) {
+        console.log(err)
+    }
 })
 app.get('/users', async (req, res) => {
     const client = new MongoClient(uri)
