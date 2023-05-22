337a61270aa96d7b57363db73cbf94eedb7cbfab
Taylor Weirtz <teweirtz@gmail.com>
2023-05-22 19:51:17 -0400

    updated chatinput and messages

--- UI/swipe-right/src/Components/ChatInput.js
+++ UI/swipe-right/src/Components/ChatInput.js
@@ -1,14 +1,37 @@
-import React, {useState} from "react";
+import React, {useState} from "react"
 import './Chat.css'
+import axios from "axios"
 
 const ChatInput = () => {
     const [textArea, setTextArea] = useState(null)
+
+    const userId = user?.user_id
+    const clickedUserId = clickedUser?.user_id
+
+    const addMessage = async () => {
+        const message = {
+            timestamp: new Date().toISOString(),
+            from_userId: userId,
+            to_userId: clickedUserId,
+            message: textArea
+        }
+
+        try {
+            await axios.post('http://localhost:8000/message', { message })
+            getUserMessages()
+            getClickedUsersMessages()
+            setTextArea("")
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         < div className = "chat_input">
             <textarea value={textArea} onChange = {(e) => setTextArea(e.target.value)}/>
-            <button>Submit</button>
+            <button className="secondary-button" onClick={addMessage}>Submit</button>
         </div>
     )
 }
 
-export default ChatInput; 
+export default ChatInput
