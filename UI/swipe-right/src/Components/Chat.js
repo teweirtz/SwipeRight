c784a620ee625b6cd0e826517f45ba12141d5367
Taylor Weirtz <teweirtz@gmail.com>
2023-05-22 20:49:53 -0400

    updated chat component

--- UI/swipe-right/src/Components/Chat.js
+++ UI/swipe-right/src/Components/Chat.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React from "react"
 import './Chat.css'
 
 
-const Chat = () => {
-    return (
-        <div className="chat_display">
-        </div>
-    )
+const Chat = ({descendingOrderMessages}) => {
+    return(
+    <>
+    <div className="chat-display">
+        {descendingOrderMessages.map((message, _index) => (
+            <div key={_index}>
+                <div className="chat-message-header">
+                    <div className="img-container">
+                        <img src={message.img} alt={message.name + ' profile'}/>
+                    </div>
+                    <p>{message.name}</p>
+                </div>
+                <p>{message.message}</p>
+            </div>
+        ))}
+    </div>
+</>
+)
 }
 
-export default Chat;
+export default Chat
