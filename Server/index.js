a7415cbb7aa2dfde6b28516d5989e46d27f44247
Taylor Weirtz <teweirtz@gmail.com>
2023-05-24 21:03:49 -0400

    changes

--- Server/index.js
+++ Server/index.js
@@ -1,16 +1,16 @@
-const express = require('express')
+import express, { json } from 'express'
 const port = 8000
-const { MongoClient } = require('mongodb')
-const { v4:uuid4 } = require('uuid')
-const jwt = require('jsonwebtoken')
-const cors = require('cors')
+import { MongoClient } from 'mongodb'
+import { v4 as uuid4 } from 'uuid'
+import { sign } from 'jsonwebtoken'
+import cors from 'cors'
 const uri = 'mongodb+srv://teweirtz:Ammo,dawg1@cluster0.tkes3cu.mongodb.net/Cluster0?retryWrites=true&w=majority'
-const bcrypt = require('bcrypt')
+import { hash, compare } from 'bcrypt'
 require('dotenv').config()
 
 const app = express()
 app.use(cors())
-app.use(express.json())
+app.use(json())
 
 app.get('/', (req, res) => {
     res.json('Hello World')
@@ -20,7 +20,7 @@ app.post('/signup', async (req, res) => {
     const client = new MongoClient(uri)
     const { email, password } = req.body
     const generateduserId = uuid4()
-    const hashedPassword = await bcrypt.hash(password, 10)
+    const hashedPassword = await hash(password, 10)
 
     try {
         await client.connect()
@@ -42,7 +42,7 @@ app.post('/signup', async (req, res) => {
         }
         const insertedUser = await users.insertOne(data)
 
-        const token = jwt.sign(insertedUser, sanitizedEmail, {
+        const token = sign(insertedUser, sanitizedEmail, {
             expiresIn: 60 *24,
         })
         res.status(201).json({token})
@@ -64,10 +64,10 @@ app.post('/login', async (req, res) => {
 
         const user = await users.findOne({email})
 
-        const correctPassword = await bcrypt.compare(password, user.hashed_password)
+        const correctPassword = await compare(password, user.hashed_password)
 
         if (user && correctPassword) {
-            const token = jwt.sign(user, email, {
+            const token = sign(user, email, {
                 expiresIn: 60 * 24
             })
             res.status(201).json({token, userId: user.user_id})
