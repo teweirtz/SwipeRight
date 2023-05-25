e21069bcc51e44fa2fe3d2196f8d5444f00a59c6
Taylor Weirtz <teweirtz@gmail.com>
2023-05-24 20:51:43 -0400

    changes

--- UI/swipe-right/src/Components/MatchesDisplay.js
+++ UI/swipe-right/src/Components/MatchesDisplay.js
@@ -1,21 +1,18 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useCookies } from "react-cookie";
 import './Components.css';
 
 
 const MatchesDisplay = ({ matches, setClickedUser }) => {
   const [matchedProfiles, setMatchedProfiles] = useState(null);
-  const [cookies, setCookie, removeCookie] = useCookies(null);
 
   const matchedUserIds = matches?.map(({ user_id }) => user_id);
-  const userId = cookies.UserId;
 
   const getMatches = async () => {
     try {
       const response = await axios.get("http://localhost:8000/users", {
         params: { userIds: JSON.stringify(matchedUserIds) },
-      });
+      })
       setMatchedProfiles(response.data);
     } catch (error) {
       console.log(error);
@@ -23,20 +20,15 @@ const MatchesDisplay = ({ matches, setClickedUser }) => {
   };
 
   useEffect(() => {
-    getMatches();
-  }, [matches]);
+    getMatches()
+  }, [])
 
-  const filteredMatchedProfiles = matchedProfiles?.filter(
-    (matchedProfile) =>
-      matchedProfile.matches.filter((profile) => profile.user_id === userId)
-        .length > 0
-  );
 
   return (
     <div className="matches-display">
-      {filteredMatchedProfiles?.map((match, _index) => (
+      {matchedProfiles?.map((match) => (
         <div
-          key={_index}
+          key={match.user_id}
           className="match-card"
           onClick={() => setClickedUser(match)}
         >
