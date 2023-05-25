d206a4a16503dee486280cf8a7565a3dec72a9b0
Taylor Weirtz <teweirtz@gmail.com>
2023-05-24 21:35:57 -0400

    chat

--- UI/swipe-right/src/Components/Chat.js
+++ UI/swipe-right/src/Components/Chat.js
@@ -1,27 +1,23 @@
-import ChatHeader from './ChatHeader';
-import MatchesDisplay from './MatchesDisplay';
-import ChatDisplay from './ChatDisplay';
-import React, { useState } from 'react';
-import './Components.css';
+import React from "react"
 
-
-const ChatContainer = ({ user }) => {
-    const [ clickedUser, setClickedUser ] = useState(null)
-
+const Chat = ({descendingOrderMessages}) => {
     return (
-        <div className="chat-container">
-            <ChatHeader user={user}/>
-
-            <div>
-                <button className="option" onClick={() => setClickedUser(null)}>Matches</button>
-                <button className="option" disabled={!clickedUser}>Chat</button>
+        <>
+            <div className="chat-display">
+                {descendingOrderMessages.map((message, _index) => (
+                    <div key={_index}>
+                        <div className="chat-message-header">
+                            <div className="img-container">
+                                <img src={message.img} alt={message.name + ' profile'}/>
+                            </div>
+                            <p>{message.name}</p>
+                        </div>
+                        <p>{message.message}</p>
+                    </div>
+                ))}
             </div>
-
-            {!clickedUser && <MatchesDisplay matches={user.matches} setClickedUser={setClickedUser}/>}
-
-            {clickedUser && <ChatDisplay user={user} clickedUser={clickedUser}/>}
-        </div>
+        </>
     )
 }
 
-export default ChatContainer;
+export default Chat
